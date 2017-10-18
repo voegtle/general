e12532e86042da3fdab6d6370294d748c0adda6a
voegtle <32686426+voegtle@users.noreply.github.com>
2017-10-18 14:39:22 -0700

    modified spherical harmonic code a bit

--- sphericalharmonic_mjv.m
+++ sphericalharmonic_mjv.m
@@ -41,3 +41,30 @@ surf(x,y,z);
 hold on 
 
 surf(x,y,-z)
+
+%%%% Here, I'm just testing out how my code performs for 
+%%%% higher order polynomials
+
+% LL=2;
+% 
+% N=legendre(LL,costhet,'norm');%
+% %We have to change the order of the output matrix to allow 
+% %easy access to its values. We use permute to do that. 
+% Nt = permute(N, [2 3 1]);
+% 
+% %Here another figure is plotted
+% figure(3)
+% %Pull values from the first 200x200 array that Nt contains
+% r = Nt(:,:,1)' * Nt(:,:,1); 
+% 
+% %The values of z and -z are plotted, similar to above. 
+% [x,y,z]= sph2cart(THET,PHI,r); 
+% 
+% surf(x,y,z);
+% hold on 
+% 
+% % surf(x,y,-z)
+% % r = Nt(:,:,2)' * Nt(:,:,1);
+% % surf(x,y,z);
+% % 
+% % surf(x,y,-z)
